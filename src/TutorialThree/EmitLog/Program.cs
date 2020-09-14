5102db4f3eb23e710745245cf46ab765d869f8ba
Luke Parker <lparkermg@outlook.com>
2020-09-14 21:13:10 +0100

    Tutorial three (#4)
    * Adds Emit and ReceiveLog projects.
    
    * Adds EmitLog code.
    
    * Finishes TutorialThree.

---
+++ src/TutorialThree/EmitLog/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace EmitLog
+{
+    internal sealed class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+                var message = args.Length > 0 ? string.Join(" ", args) : "Info: Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange: "logs",
+                    routingKey: string.Empty,
+                    basicProperties: null,
+                    body: body);
+                Console.WriteLine($"[X] Sent log: {message}");
+            }
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
