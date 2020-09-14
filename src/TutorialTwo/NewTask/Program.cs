b0f552e74f0a48a068583fa6c0a3a192ef0b8ec8
Luke Parker <lparkermg@outlook.com>
2020-09-14 19:26:38 +0100

    Adds tweaked send code to NewTask.

--- src/TutorialTwo/NewTask/Program.cs
+++ src/TutorialTwo/NewTask/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace NewTask
 {
-    class Program
+    internal sealed class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var message = (args.Length > 0) ? string.Join(" ", args) : "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(
+                    exchange: string.Empty,
+                    routingKey: "task_queue",
+                    basicProperties: properties,
+                    body: body);
+                Console.WriteLine($"[X] Sent {message}");
+            }
+
+            Console.WriteLine("Press [Any Key] to exit.");
+            Console.ReadKey();
         }
     }
 }
