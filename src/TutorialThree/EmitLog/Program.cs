d91f6198657fce2571ff165872d0ea5984ba72de
Luke Parker <lparkermg@outlook.com>
2020-09-14 20:53:54 +0100

    Adds EmitLog code.

--- src/TutorialThree/EmitLog/Program.cs
+++ src/TutorialThree/EmitLog/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace EmitLog
 {
-    class Program
+    internal sealed class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+                var message = args.Length > 0 ? string.Join(" ", args) : "Info: Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange: "logs",
+                    routingKey: string.Empty,
+                    basicProperties: null,
+                    body: body);
+                Console.WriteLine($"[X] Sent log: {message}");
+            }
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
         }
     }
 }
