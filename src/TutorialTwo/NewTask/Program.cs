e64c4ebf0dcef479c875f374ef944795066b2fe2
Luke Parker <lparkermg@outlook.com>
2020-09-14 20:12:45 +0100

    Tutorial two (#3)
    * Adds NewTask and Worker projects with RabbitMQ.
    
    * Adds tweaked send code to NewTask.
    
    * Added worker code. Tweaked queue names in NewTask.

---
+++ src/TutorialTwo/NewTask/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace NewTask
+{
+    internal sealed class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "task_queue",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var message = (args.Length > 0) ? string.Join(" ", args) : "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                for(var i = 0; i < 10; i++)
+                {
+                    channel.BasicPublish(
+                        exchange: string.Empty,
+                        routingKey: "task_queue",
+                        basicProperties: properties,
+                        body: body);
+                    Console.WriteLine($"[X] Sent {message} {i}");
+                }
+                
+            }
+        }
+    }
+}
---
+++ src/TutorialTwo/Worker/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Worker
+{
+    internal sealed class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "task_queue",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+                channel.BasicQos(0, 1, false);
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) => 
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine($"[X] Recieved {message}");
+
+                    int dots = message.Split('.').Length - 1;
+
+                    Thread.Sleep(dots * 1000);
+
+                    Console.WriteLine("[X] Done");
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+
+                channel.BasicConsume(
+                    queue: "task_queue",
+                    autoAck: false,
+                    consumer: consumer);
+                Console.ReadKey();
+            }
+        }
+    }
+}
