40c7de73fc9d88ef504718ece6f61ab27008dabd
Luke Parker <lparkermg@outlook.com>
2020-09-14 18:34:32 +0100

    Completed publisher(send) code.

--- Send/Program.cs
+++ Send/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace Send
 {
-    class Program
+    internal sealed class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var message = "Hellow World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange: string.Empty,
+                    routingKey:"hello",
+                    body: body);
+                Console.WriteLine($"[X] Sent {message}");
+            }
+
+            Console.WriteLine("Press [Any Key] to exit.");
+            Console.ReadKey();
         }
     }
 }
