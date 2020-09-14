a92c4c49bf54227f70f011dfdfa90c051832dc24
Luke Parker <lparkermg@outlook.com>
2020-09-14 18:54:03 +0100

    Tutorial One (#1)
    * Added Send and Recieve programs.
    
    * Completed publisher(send) code.
    
    * Implemented Receive program.

---
+++ Receive/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Receive
+{
+    internal sealed class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) => 
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine($"[X] Recieved {message}");
+                };
+
+                channel.BasicConsume(
+                    queue: "hello",
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine("Press [Any Key] to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Send/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    internal sealed class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var message = "Hellow World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange: string.Empty,
+                    routingKey:"hello",
+                    body: body);
+                Console.WriteLine($"[X] Sent {message}");
+            }
+
+            Console.WriteLine("Press [Any Key] to exit.");
+            Console.ReadKey();
+        }
+    }
+}
