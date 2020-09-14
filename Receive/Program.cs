8f77eed9b607399dc56ad3e911801101c5a876c0
Luke Parker <lparkermg@outlook.com>
2020-09-14 18:49:28 +0100

    Implemented Receive program.

--- Receive/Program.cs
+++ Receive/Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace Receive
 {
-    class Program
+    internal sealed class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) => 
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine($"[X] Recieved {message}");
+                };
+
+                channel.BasicConsume(
+                    queue: "hello",
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine("Press [Any Key] to exit.");
+                Console.ReadKey();
+            }
         }
     }
 }
